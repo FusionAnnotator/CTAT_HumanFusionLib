f4b44c750986466308df31a256d52e5ca7bd97ba
Brian Haas <brianjohnhaas@gmail.com>
2017-11-19 15:31:35 -0500

    incorporate gene naming info directly from HGNC

---
+++ fusion_lib_data/HGNC.pm
@@ -0,0 +1,37 @@
+package HGNC;
+
+use strict;
+use warnings;
+use Carp;
+
+
+sub load_data {
+    my ($annotations_href, $hgnc_file_gz) = @_;
+
+    open(my $fh, "gunzip -c $hgnc_file_gz | ") or die "Error, cannot read from file: $hgnc_file_gz";
+    my $header = <$fh>;
+    while (<$fh>) {
+        chomp;
+        my @x = split(/\t/);
+        my ($symbol, $name, $gene_type) = ($x[1], $x[2], $x[3]);
+
+        my $annotation = "$name,[$gene_type]";
+        $annotations_href->{$symbol}->{$annotation} = 1;
+        
+        my $alias_text = $x[8];
+        if ($alias_text) {
+            $alias_text =~ s/\"//g;
+            my @aliases = split(/\|/, $alias_text);
+            foreach my $alias (@aliases) {
+                if (! exists $annotations_href->{$alias}) {
+                    $annotations_href->{$alias}->{$annotation} = 1;
+                }
+            }
+        }
+    }
+    close $fh;
+    
+}
+
+1; #EOM
+
--- fusion_lib_data/README.md
+++ fusion_lib_data/README.md
@@ -3,3 +3,7 @@ Data sources:
 (will add in the earlier ones here eventually)
 
 allonco_20130923.tsv	http://www.bushmanlab.org/links/genelists
+
+
+gene symbol annotations:   ftp://ftp.ebi.ac.uk/pub/databases/genenames/new/tsv/hgnc_complete_set.txt
+
