1e6c7f3c87fa65862ec481ab7b791b3e57d6f853
Brian Haas <brianjohnhaas@gmail.com>
2019-03-24 12:31:49 -0400

    update for gtex

---
+++ fusion_lib_data/loaders/GTEx_Blacklist_Mar2019.pm
@@ -0,0 +1,34 @@
+package GTEx_Blacklist_Mar2019;
+
+## bhaas - ran starF / FI on all of GTEx
+
+use strict;
+use warnings;
+use Carp;
+
+sub load_data {
+    my ($annotations_href, $gtex_normals) = @_;
+
+    print STDERR "-parsing GTEx_Blacklist_Mar2019 $gtex_normals\n";
+    
+    open (my $fh, "gunzip -c $gtex_normals | ") or confess "Error, cannot open file $gtex_normals";
+
+    while (<$fh>) {
+        chomp;
+        my ($fusion, $GTEx_annot) = split(/\t/);
+        
+        $annotations_href->{$fusion}->{COMPLEX}->{"GTEx"} = "GTEx_Recurrent:{$GTEx_annot}";
+        
+        $annotations_href->{$fusion}->{SIMPLE}->{"GTEx"} = 1;
+        
+    }
+    close $fh;
+    
+    return;
+}
+
+
+1; #EOM
+
+
+    
