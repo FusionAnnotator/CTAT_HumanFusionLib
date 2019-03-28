2de2b5b8e3463dbda06098e389f09b799d0eef5f
Brian Haas <brianjohnhaas@gmail.com>
2019-03-28 14:36:20 -0400

    update

--- fusion_lib_data/loaders/CCLE_StarF2019.pm
+++ fusion_lib_data/loaders/CCLE_StarF2019.pm
@@ -8,12 +8,31 @@ sub load_data {
     my ($annotations_href, $fusions_file) = @_;
     
     print STDERR "-parsing $fusions_file\n";
+
+    my %CCLE_fusions;
     
-    open (my $fh, "gunzip -c $fusions_file | ") or die "Error, cannot open file: $fusions_file";
+    open (my $fh, $fusions_file) or die "Error, cannot open file: $fusions_file";
+    my $header = <$fh>;
     while(<$fh>) {
+        unless (/\w/) { next; }
         chomp;
-        my ($fusion, $ccle_info) = split(/\t/);
-        $annotations_href->{$fusion}->{COMPLEX}->{"CCLE_StarF2019"} = $ccle_info;
+        my ($ccle, $cell_line, $fusion_name) = split(/\t/);
+
+        unless ($fusion_name) {
+            die "Error, $_";
+        }
+        
+        push (@{$CCLE_fusions{$fusion_name}}, $cell_line);
+    }
+    close $fh;
+
+    foreach my $fusion (keys %CCLE_fusions) {
+
+        my @cell_lines = sort @{$CCLE_fusions{$fusion}};
+
+        my $annot_str = join("|", @cell_lines);
+
+        $annotations_href->{$fusion}->{COMPLEX}->{"CCLE_StarF2019"} = $annot_str;
         $annotations_href->{$fusion}->{SIMPLE}->{"CCLE_StarF2019"} = 1;
     }
     
